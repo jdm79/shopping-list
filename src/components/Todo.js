64d3ca7199bf2fc6df2d936e106290965eff475a
jdm79 <james.malvern@gmail.com>
2023-08-17 21:45:50 +0100

    oof. remove time ago span for now

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -12,6 +12,7 @@ export default function Todo() {
   let currentDate = new Date();
   let year = currentDate.getFullYear();
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  let d = currentDate.getDay();
   const timeAgo = new TimeAgo("en-US");
   console.log(timeAgo.format(Date.now(), "round-minute"));
   console.log(currentDate.toLocaleTimeString().replace(/(.*)\D\d+/, "$1"));
