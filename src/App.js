2a0b871be92660b04518bc08f76e3f26755de168
jdm79 <james.malvern@gmail.com>
2023-08-17 23:03:29 +0100

    add clear button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Todo from "./components/Todo";
 function App() {
   return (
     <div className=''>
-      <button>Press to clear</button>
       <Todo />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <button>Press to clear</button>
+
     <App />
   </React.StrictMode>
 );
